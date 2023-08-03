fe1486c776e8e103383fdeec75f2c3e1fb28848f
seoyeon750 <67733350+seoyeon750@users.noreply.github.com>
2023-08-03 13:57:30 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,12 +1,11 @@
 import streamlit as st
-from snwoflake.snowpark.session import Session
-from snowflake.snowpark import functions as F
-from snowflake.snowpark.types import *
 
-ss = st.session_state
-session = ""
-if not ss:
-  ss.pressed_first_button = False
-
-#with st.container():
-#  if 
+st.set_page_config(
+  page_title = "Snowflake Health Check App",
+  page_icon = "*"
+  initial_sidebar_state = "auto",
+  menu_items = {
+    "Get Help" : "https://developers.snowflake.com",
+    "About" : "The Application is built by Snowflake Data Superhero - [Divyansh Saxena](https://www.linkedin.com/in/divyanshsaxena/) . The source code for this application can be accessed on [GitHub](https://github.com/divyanshsaxena11/sfguide-snowpark-streamlit-snowflake-healthcheck)",
+  },
+)
