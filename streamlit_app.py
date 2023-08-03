5473225a7fb016da897ba580c9df755c0a2a66b0
seoyeon750 <67733350+seoyeon750@users.noreply.github.com>
2023-08-03 14:01:12 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,14 @@ st.set_page_config(
     "About" : "The Application is built by Snowflake Data Superhero - [Divyansh Saxena](https://www.linkedin.com/in/divyanshsaxena/) . The source code for this application can be accessed on [GitHub](https://github.com/divyanshsaxena11/sfguide-snowpark-streamlit-snowflake-healthcheck)",
   },
 )
+
+ss = st_session_state
+session = ""
+if not ss:
+  ss.pressed_first_button = False
+
+with st.sidebar:
+  SF_ACCOUNT = st.text_input("Enter Your Snowflake Account [<account_details>.snowflakecomputing.com] :")
+  SF_USR = st.text_input("Snowflake USER ( s2oy2on ):")
+  SF_PWD = st.text_input("Snowflake password:", type="password")
+  conn = {"ACCOUNT": SF_ACCOUNT, "USER": SF_USR, "PASSWORD": SF_PWD}
